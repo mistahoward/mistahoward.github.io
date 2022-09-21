8d1226c7b6ae2ca7e6e50715a8c1c257aaedf9ec
mistahoward <me@alexhoward.dev>
2022-09-20 21:04:24 -0500

    More ESlint config
    Also adding linting to the start script

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,37 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ["plugin:react/recommended", "standard-with-typescript", "airbnb"],
+	parser: "@typescript-eslint/parser",
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+		project: "./tsconfig.json",
+	},
+	plugins: ["react", "@typescript-eslint"],
+	rules: {
+		"space-before-function-paren": "off",
+		"no-tabs": "off",
+		"indent": ["error", "tab"],
+		"@typescript-eslint/semi": "off",
+		"@typescript-eslint/indent": ["error", "tab"],
+    "react/jsx-filename-extension": [1, { extensions: [".tsx", ".jsx"] }],
+    "react/jsx-indent": [1, 'tab'],
+    "react/react-in-jsx-scope": "off",
   },
-  extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript',
-    'airbnb'
-  ],
-  parser: '@typescript-eslint/parser',
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: './tsconfig.json'
-  },
-  plugins: [
-    'react',
-    '@typescript-eslint'
-  ],
-  rules: {
-    spaceBeforeFunctionParen: "off",
-    noTabs: "off",
-    indent: ["error", "tab"]
-  }
-}
+  globals: {
+    "React": true,
+    "google": true,
+    "mount": true,
+    "mountWithRouter": true,
+    "shallow": true,
+    "shallowWithRouter": true,
+    "context": true,
+    "expect": true,
+    "jsdom": true,
+    "JSX": true,
+},
+};
