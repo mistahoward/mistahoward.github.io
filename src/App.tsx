d41452997f6c0a5dafbee529240597fc932e14f2
mistahoward <me@alexhoward.dev>
2022-09-20 21:51:16 -0500

    Beginning Bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
+import { Container, Row } from 'react-bootstrap';
 import Header from './ui/Header';
 import Body from './ui/Body';
 import Footer from './ui/Footer';
 
 function App(): JSX.Element {
 	return (
-		<>
-			<Header />
-			<Body />
-			<Footer />
-		</>
+		<Container>
+			<Row>
+				<Header />
+			</Row>
+			<Row>
+				<Body />
+			</Row>
+			<Row>
+				<Footer />
+			</Row>
+		</Container>
 	);
 }
 
--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -1,5 +1,18 @@
+import { Row, Col } from 'react-bootstrap';
+import '../assets/css/header.css';
+
 function Header(): JSX.Element {
-	return <div>Header</div>;
+	return (
+		<Col id="main-nav">
+			<Row>
+				<Col>Home</Col>
+				<Col>About Me</Col>
+				<Col><h1 className="display-6">AH</h1></Col>
+				<Col>Portfolio</Col>
+				<Col>Dark Mode Toggle</Col>
+			</Row>
+		</Col>
+	);
 }
 
 export default Header;
