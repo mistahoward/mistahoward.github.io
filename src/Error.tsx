5af27bf8622ba68e112312790f566d0cde15e56d
mistahoward <me@alexhoward.dev>
2022-09-20 22:05:02 -0500

    Auto stash before revert of "Adding React Router"
    Stashing these changes, decided against using react router.

---
+++ src/Error.tsx
@@ -0,0 +1,5 @@
+function Error(): JSX.Element {
+	return <div>it borked</div>;
+}
+
+export default Error;
--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -2,12 +2,20 @@ import { Row, Col } from 'react-bootstrap';
 import '../assets/css/header.css';
 
 function Header(): JSX.Element {
+	const router = createBrowserRouter([
+		{
+			path: 'Home',
+			element: <Home />,
+		},
+	]);
 	return (
 		<Col id="main-nav">
 			<Row>
 				<Col>Home</Col>
 				<Col>About Me</Col>
-				<Col><h1 className="display-6">AH</h1></Col>
+				<Col>
+					<h1 className="display-6">AH</h1>
+				</Col>
 				<Col>Portfolio</Col>
 				<Col>Dark Mode Toggle</Col>
 			</Row>
