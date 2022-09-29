9f2bcc22cb34e5536b98b5f04689c7bc722966dc
Alex Howard <me@alexhoward.dev>
2022-09-29 00:46:50 -0500

    Finishing up some css margins

--- src/ui/Home.tsx
+++ src/ui/Home.tsx
@@ -13,13 +13,14 @@ function Home(): JSX.Element {
 	return (
 		<Row id="home">
 			<Col>
-				<Row>
-					<Col className="mb-3">
+				<Row className="g-0">
+					<Col lg={2} xl={2} />
+					<Col sm={12} md={6} lg={4} xl={3} className="mb-3 portrait-container">
 						<Lottie id="wave" animationData={wave} loop />
 						<img id="portrait" src={Headshot} alt="Portrait of Alex Howard" />
 					</Col>
-					<Col>
-						<Row>
+					<Col sm={12} md={6} lg={6} xl={7} className="text-center text-md-start">
+						<Row className="g-0">
 							<Col>
 								<h1 className="display-4 text-nowrap">
 									Hey! I&rsquo;m
@@ -38,44 +39,60 @@ function Home(): JSX.Element {
 								<h1 className="display-6 text-nowrap">Front End Developer</h1>
 							</Col>
 						</Row>
-						<Row className="larger-text text-muted g-0">
-							<Col xs={2}>
-								🥤
-							</Col>
-							<Col>
-								Fueled By Dr. Pepper
-							</Col>
-						</Row>
-						<Row className="larger-text text-muted g-0">
-							<Col xs={2}>
-								🇺🇸
-							</Col>
-							<Col>
-								Located in Missouri
+						<Row className="larger-text text-muted g-0 justify-content-center">
+							<Col xs={8} md={12}>
+								<Row>
+									<Col xs={2}>
+										🥤
+									</Col>
+									<Col xs={10} className="text-nowrap">
+										Fueled By Dr. Pepper
+									</Col>
+								</Row>
 							</Col>
 						</Row>
-						<Row className="larger-text text-muted g-0">
-							<Col xs={2}>
-								💼
-							</Col>
-							<Col xs={10} className="text-nowrap">
-								Developer at
-								{' '}
-								<a target="_blank" aria-label="PowerShades Home Page" rel="noreferrer" href="http://powershades.com/">PowerShades</a>
+						<Row className="larger-text text-muted g-0 justify-content-center">
+							<Col xs={8} md={12}>
+								<Row>
+									<Col xs={2}>
+										🇺🇸
+									</Col>
+									<Col xs={10} className="text-nowrap">
+										Located in Missouri
+									</Col>
+								</Row>
 							</Col>
 						</Row>
-						<Row className="display-6 mt-2">
-							<Col>
-								<a target="_blank" aria-label="Alex Howard's Github" rel="noreferrer" href="https://github.com/mistahoward"><FiGithub className="link" /></a>
+						<Row className="larger-text text-muted g-0 justify-content-center">
+							<Col xs={8} md={12}>
+								<Row>
+									<Col xs={2}>
+										💼
+									</Col>
+									<Col xs={10} className="text-nowrap">
+										Developer at
+										{' '}
+										<a target="_blank" aria-label="PowerShades Home Page" rel="noreferrer" href="http://powershades.com/">PowerShades</a>
+									</Col>
+								</Row>
 							</Col>
-							<Col>
-								<a target="_blank" aria-label="Alex Howard's LinkedIn" rel="noreferrer" href="https://www.linkedin.com/in/jalexhoward/"><FiLinkedin className="link" /></a>
-							</Col>
-							<Col>
-								<a target="_blank" aria-label="Alex Howard's Twitter" rel="noreferrer" href="https://twitter.com/mista_howard"><FiTwitter className="link" /></a>
-							</Col>
-							<Col>
-								<a target="_blank" aria-label="Alex Howard's Email" rel="noreferrer" href="mailto:me@alexhoward.dev"><FiMail className="link" /></a>
+						</Row>
+						<Row className="display-6 mt-2 g- justify-content-center justify-content-lg-start">
+							<Col sm={10} md={12} lg={8} xl={7}>
+								<Row className="g-0">
+									<Col>
+										<a target="_blank" aria-label="Alex Howard's Github" rel="noreferrer" href="https://github.com/mistahoward"><FiGithub className="link" /></a>
+									</Col>
+									<Col>
+										<a target="_blank" aria-label="Alex Howard's LinkedIn" rel="noreferrer" href="https://www.linkedin.com/in/jalexhoward/"><FiLinkedin className="link" /></a>
+									</Col>
+									<Col>
+										<a target="_blank" aria-label="Alex Howard's Twitter" rel="noreferrer" href="https://twitter.com/mista_howard"><FiTwitter className="link" /></a>
+									</Col>
+									<Col>
+										<a target="_blank" aria-label="Alex Howard's Email" rel="noreferrer" href="mailto:me@alexhoward.dev"><FiMail className="link" /></a>
+									</Col>
+								</Row>
 							</Col>
 						</Row>
 					</Col>
