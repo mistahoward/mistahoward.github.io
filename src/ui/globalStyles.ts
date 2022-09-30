15acff6da14d3432a64ba4f5c2ac02d56ff6bd7e
Alex Howard <me@alexhoward.dev>
2022-09-29 21:57:48 -0500

    Removing darkmode transition

--- src/ui/globalStyles.ts
+++ src/ui/globalStyles.ts
@@ -7,7 +7,6 @@ const GlobalStyles = createGlobalStyle`
   body {
     background: ${({ theme }) => theme.body};
     color: ${({ theme }) => theme.text};
-    transition: all 0.50s linear;
   }
   .main-color {
     color: #E86100;
