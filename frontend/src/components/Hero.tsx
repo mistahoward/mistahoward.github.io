5529af9fc0d73fe9d57cb2137afcd72dbe5e0829
Alex Howard <me@alexhoward.dev>
2025-07-12 09:35:10 -0500

    feat: adding more rings - probably swapping to particles though...

--- frontend/src/components/Hero.tsx
+++ frontend/src/components/Hero.tsx
@@ -4,6 +4,8 @@ export const Hero = () => (
 			<div className="hero-portrait-ring ring1"></div>
 			<div className="hero-portrait-ring ring3"></div>
 			<div className="hero-portrait-ring ring4"></div>
+			<div className="hero-portrait-ring ring5"></div>
+			<div className="hero-portrait-ring ring6"></div>
 			<img src="/portrait.jpg" alt="Hand-drawn portrait of Alex" loading="lazy" />
 		</div>
 	</section>
