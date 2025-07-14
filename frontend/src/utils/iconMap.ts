12804bc57164bb75018455180923fea9f6390225
Alex Howard <me@alexhoward.dev>
2025-07-13 23:25:18 -0500

    dev: adding more icons for experience section

--- frontend/src/utils/iconMap.ts
+++ frontend/src/utils/iconMap.ts
@@ -1,8 +1,36 @@
 import { h } from "preact";
 import { TbBrandCSharp } from "react-icons/tb";
-import { SiBlazor, SiTypescript, SiJavascript, SiReact, SiXml, SiGit, SiSqlite, SiConfluence } from "react-icons/si";
-import { FaRecycle } from "react-icons/fa";
+import {
+	SiBlazor,
+	SiTypescript,
+	SiJavascript,
+	SiReact,
+	SiXml,
+	SiGit,
+	SiSqlite,
+	SiConfluence,
+	SiDotnet,
+	SiRedux,
+	SiJquery,
+	SiJest,
+	SiRedis
+} from "react-icons/si";
+import {
+	FaRecycle,
+	FaHtml5,
+	FaCss3Alt,
+	FaSass,
+	FaPython,
+	FaLaravel,
+	FaRust,
+	FaBootstrap,
+	FaTheaterMasks,
+	FaDocker
+} from "react-icons/fa";
 import { VscAzureDevops } from "react-icons/vsc";
+import { TbBrandReactNative } from "react-icons/tb";
+import { RiPhpFill } from "react-icons/ri";
+import { CgCPlusPlus } from "react-icons/cg";
 
 export const iconMap: Record<string, any> = {
 	TbBrandCSharp,
@@ -15,7 +43,24 @@ export const iconMap: Record<string, any> = {
 	SiGit,
 	SiSqlite,
 	SiConfluence,
-	FaRecycle
+	FaRecycle,
+	FaHtml5,
+	FaCss3Alt,
+	FaSass,
+	FaPython,
+	TbBrandReactNative,
+	RiPhpFill,
+	FaLaravel,
+	CgCPlusPlus,
+	SiDotnet,
+	SiRedux,
+	SiJquery,
+	FaRust,
+	FaBootstrap,
+	FaTheaterMasks,
+	SiJest,
+	FaDocker,
+	SiRedis
 };
 
 export const iconNameToLabel: Record<string, string> = {
@@ -30,6 +75,23 @@ export const iconNameToLabel: Record<string, string> = {
 	SiSqlite: "SQLite",
 	SiConfluence: "Confluence",
 	FaRecycle: "Agile Methodology",
+	FaHtml5: "HTML5",
+	FaCss3Alt: "CSS3",
+	FaSass: "SCSS",
+	FaPython: "Python",
+	TbBrandReactNative: "React Native",
+	RiPhpFill: "PHP",
+	FaLaravel: "Laravel",
+	CgCPlusPlus: "C++",
+	SiDotnet: ".NET",
+	SiRedux: "Redux",
+	SiJquery: "jQuery",
+	FaRust: "Rust",
+	FaBootstrap: "Bootstrap",
+	FaTheaterMasks: "Playwright",
+	SiJest: "Jest",
+	FaDocker: "Docker",
+	SiRedis: "Redis"
 };
 
 export const renderIcon = (iconName: string, size = 24, className = "me-2") => {
